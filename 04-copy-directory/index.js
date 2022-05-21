e08557d8bd2da4b40350821f16587137ab002e61
zvenrm <zvennnnnn@gmail.com>
2022-05-21 09:00:18 +0300

    fix: refactor the code of 04 task with promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,24 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const main = path.join(__dirname, 'files')
 const copy = path.join(__dirname, 'files-copy')
 
-const copyDir = (main, copy) => {
-  fs.mkdir(copy, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log('Directory created successfully!');
-  });
-
-  fs.readdir(copy, (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        fs.access(path.join(main, file), (err) => {
-          if (err) {
-            fs.rm(path.join(copy, file), { recursive: true, force: true}, (err)   => {
-              if (err) throw err;
-            });
-          }
-        });
-      })
-    }
+async function copyDir(main, copy){
+  await fs.mkdir(copy, {recursive: true})
+  const readCopy = await fs.readdir(copy)
+  readCopy.forEach(async e => {
+    fs.access(path.join(main, e)).catch(() => {
+      fs.rm(path.join(copy, e), {recursive: true})
+    })
+    
   })
-
-  fs.readdir(main, {withFileTypes: true}, (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        if(file.isFile()){
-          fs.copyFile(path.join(main, file.name), path.join(copy, file.name), (err) => {
-            if (err) throw err;
-          })
-        }
-        if(file.isDirectory()) {
-          copyDir(path.join(main, file.name), path.join(copy, file.name))
-        }
-      })
+  const readMain = await fs.readdir(main, {withFileTypes: true})
+  readMain.forEach(async e => {
+    if(e.isFile()){
+      fs.copyFile(path.join(main, e.name), path.join(copy, e.name))
+    }
+    if(e.isDirectory()) {
+      copyDir(path.join(main, e.name), path.join(copy, e.name))
     }
   })
 }
